e697e675803240851030d5de98b0993945737791
Dvd848 <Dvd848@users.noreply.github.com>
2023-10-05 21:14:36 +0300

    Update license & README

--- p4checkout/p4checkout.py
+++ p4checkout/p4checkout.py
@@ -1,3 +1,34 @@
+"""A standalone GUI utility offering Perforce users the ability to check out files.
+
+While this ability is available out of the box by directly using the official Perforce visual 
+client (p4v), it's convenient to integrate it into the user's IDE. Most popular IDEs have 
+an official Perforce plugin, but those that don't can use this utility to easily check out 
+files before editing them.
+
+Source:
+    https://github.com/Dvd848/p4checkout
+
+License:
+    MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
 from typing import Optional, List, Tuple, Union, Callable, Dict
 from collections.abc import Iterator
 from collections import namedtuple
